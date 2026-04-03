a4f847f3819dc9077ae7410afb8ecea5c68d7547
Thumati Sai Abhiram <abhiramtumati21@gmail.com>
2026-04-03 12:09:40 +0530

    Fix: lowercase model filenames for Linux

--- backend/routes/groups.js
+++ backend/routes/groups.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Group = require('../models/Group');
+const Group = require('../models/group');
 const auth = require('../middleware/auth');
 
 // Get all groups
